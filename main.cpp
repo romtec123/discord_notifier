eae83b4ef54bf32d6adaf62c5c7fe7f91fcab973
romtec123 <julianlevin11@gmail.com>
2023-03-14 11:16:50 -0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/****************************************************************
+* Author: romtec123                                             *
+* Date: March 14 2023                                           *
+* File: main.cpp                                                *
+*                                                               *
+* Description: This utility allows the user to add webhooks     *
+* with a name, list webhooks, update webhooks, and send         *
+* messages through a shell.                                     *
+****************************************************************/
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
